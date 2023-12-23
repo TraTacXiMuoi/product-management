f3d2976cfb20730634aa1f3ed04c4cd94ae1be84
Đặng Phương Nam <61955753+namdp1999@users.noreply.github.com>
2023-12-23 22:39:21 +0700

    6.4. Làm trang danh sách sản phẩm bên Client

--- controllers/client/product.controller.js
+++ controllers/client/product.controller.js
@@ -1,21 +1,21 @@
+const Product = require("../../models/product.model");
+
 // [GET] /products/
-module.exports.index = (req, res) => {
+module.exports.index = async (req, res) => {
+  const products = await Product.find({
+    status: "active",
+    deleted: false
+  });
+
+  for (const item of products) {
+    item.priceNew = item.price * (1 - item.discountPercentage/100);
+    item.priceNew = item.priceNew.toFixed(0);
+  }
+
+  // console.log(products);
+
   res.render("client/pages/products/index", {
-    pageTitle: "Danh sách sản phẩm"
+    pageTitle: "Danh sách sản phẩm",
+    products: products
   });
 }
-
-// // [GET] /products/detail
-// module.exports.detail = (req, res) => {
-//   res.send("Trang chi tiết sản phẩm");
-// }
-
-// // [GET] /products/edit
-// module.exports.edit = (req, res) => {
-  
-// }
-
-// // [GET] /products/create
-// module.exports.create = (req, res) => {
-  
-// }
