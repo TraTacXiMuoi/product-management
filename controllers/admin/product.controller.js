04c338907f7562c018a1ba93c3fa5d0094892cfb
Đặng Phương Nam <61955753+namdp1999@users.noreply.github.com>
2024-01-04 21:44:55 +0700

    4.2. Xóa mềm

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -81,3 +81,25 @@ module.exports.changeMulti = async (req, res) => {
 
   res.redirect("back");
 }
+
+// [DELETE] /admin/products/delete/:id
+module.exports.deleteItem = async (req, res) => {
+  try {
+    const id = req.params.id;
+
+    // await Product.deleteOne({
+    //   _id: id
+    // });
+
+    await Product.updateOne({
+      _id: id
+    }, {
+      deleted: true,
+      deletedAt: new Date()
+    });
+  } catch (error) {
+    console.log(error);
+  }
+
+  res.redirect("back");
+}
