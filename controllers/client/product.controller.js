9ed771045578e88a88bf0eb53df18a7e3d4ec884
Đặng Phương Nam <61955753+namdp1999@users.noreply.github.com>
2024-01-09 22:38:33 +0700

    4. Làm trang chi tiết sản phẩm bên client

--- controllers/client/product.controller.js
+++ controllers/client/product.controller.js
@@ -21,3 +21,25 @@ module.exports.index = async (req, res) => {
     products: products
   });
 }
+
+// [GET] /products/:slug
+module.exports.detail = async (req, res) => {
+  try {
+    const slug = req.params.slug;
+
+    const product = await Product.findOne({
+      slug: slug,
+      deleted: false,
+      status: "active"
+    });
+
+    console.log(product);
+
+    res.render("client/pages/products/detail", {
+      pageTitle: product.title,
+      product: product
+    });
+  } catch (error) {
+    res.redirect("/");
+  }
+}
--- routes/client/product.route.js
+++ routes/client/product.route.js
@@ -5,10 +5,6 @@ const controller = require("../../controllers/client/product.controller");
 
 router.get("/", controller.index);
 
-// router.get("/detail", controller.detail);
-
-// router.get("/edit", controller.edit);
-
-// router.get("/create", controller.create);
+router.get("/:slug", controller.detail);
 
 module.exports = router;
