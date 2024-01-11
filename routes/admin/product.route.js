1ee9b914f69d630344ee5497f138927cb4e9241b
Đặng Phương Nam <61955753+namdp1999@users.noreply.github.com>
2024-01-11 22:20:46 +0700

    6. Đẩy file tĩnh lên Cloud (Tối ưu)

--- routes/admin/product.route.js
+++ routes/admin/product.route.js
@@ -1,25 +1,17 @@
 const express = require("express");
 const router = express.Router();
 const multer = require("multer");
-const cloudinary = require("cloudinary").v2;
-const streamifier = require("streamifier");
 
 // const storageMulter = require("../../helpers/storage-multer.helper");
 
 // const upload = multer({ storage: storageMulter() });
 
-// cloudinary
-cloudinary.config({
-  cloud_name: process.env.CLOUD_NAME,
-  api_key: process.env.CLOUD_KEY,
-  api_secret: process.env.CLOUD_SECRET,
-});
-// End cloudinary
-
 const upload = multer();
 
 const controller = require("../../controllers/admin/product.controller");
 const validate = require("../../validates/admin/product.validate");
+
+const uploadCloud = require("../../middlewares/admin/uploadCloud.middleware");
 
 router.get("/", controller.index);
 
@@ -34,32 +26,7 @@ router.get("/create", controller.create);
 router.post(
   "/create",
   upload.single("thumbnail"),
-  function (req, res, next) {
-    if (req.file) {
-      let streamUpload = (req) => {
-        return new Promise((resolve, reject) => {
-          let stream = cloudinary.uploader.upload_stream((error, result) => {
-            if (result) {
-              resolve(result);
-            } else {
-              reject(error);
-            }
-          });
-
-          streamifier.createReadStream(req.file.buffer).pipe(stream);
-        });
-      };
-
-      async function upload(req) {
-        let result = await streamUpload(req);
-        req.body[req.file.fieldname] = result.url;
-        next();
-      }
-      upload(req);
-    } else {
-      next();
-    }
-  },
+  uploadCloud.uploadSingle,
   validate.createPost,
   controller.createPost
 );
@@ -69,32 +36,7 @@ router.get("/edit/:id", controller.edit);
 router.patch(
   "/edit/:id",
   upload.single("thumbnail"),
-  function (req, res, next) {
-    if (req.file) {
-      let streamUpload = (req) => {
-        return new Promise((resolve, reject) => {
-          let stream = cloudinary.uploader.upload_stream((error, result) => {
-            if (result) {
-              resolve(result);
-            } else {
-              reject(error);
-            }
-          });
-
-          streamifier.createReadStream(req.file.buffer).pipe(stream);
-        });
-      };
-
-      async function upload(req) {
-        let result = await streamUpload(req);
-        req.body[req.file.fieldname] = result.url;
-        next();
-      }
-      upload(req);
-    } else {
-      next();
-    }
-  },
+  uploadCloud.uploadSingle,
   validate.createPost,
   controller.editPatch
 );
