be028c0fa3a600a6184941340a483e2648923174
Đặng Phương Nam <61955753+namdp1999@users.noreply.github.com>
2024-01-13 23:35:11 +0700

    3. Xây dựng phần quản lý danh mục sản phẩm (Phân cấp tree)

--- controllers/admin/product-category.controller.js
+++ controllers/admin/product-category.controller.js
@@ -1,6 +1,8 @@
 const ProductCategory = require("../../models/product-category.model");
 const systemConfig = require("../../config/system");
 
+const createTreeHelper = require("../../helpers/create-tree.helper");
+
 // [GET] /admin/products-category
 module.exports.index = async (req, res) => {
   const records = await ProductCategory.find({
@@ -15,8 +17,17 @@ module.exports.index = async (req, res) => {
 
 // [GET] /admin/products-category/create
 module.exports.create = async (req, res) => {
+  const records = await ProductCategory.find({
+    deleted: false,
+  });
+
+  const newRecords = createTreeHelper(records);
+
+  console.log(newRecords);
+
   res.render("admin/pages/products-category/create", {
-    pageTitle: "Thêm mới danh mục sản phẩm"
+    pageTitle: "Thêm mới danh mục sản phẩm",
+    records: newRecords
   });
 };
 
