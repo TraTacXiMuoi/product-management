cb756e42f82983aad5644bedab25a993ec1a61b7
Đặng Phương Nam <61955753+namdp1999@users.noreply.github.com>
2024-01-20 23:04:57 +0700

    1.3. Làm trang tạo tài khoản

--- controllers/admin/account.controller.js
+++ controllers/admin/account.controller.js
@@ -1,4 +1,10 @@
+const md5 = require('md5');
 const Account = require("../../models/account.model");
+const Role = require("../../models/role.model");
+
+const generateHelper = require("../../helpers/generate.helper");
+
+const systemConfig = require("../../config/system");
 
 // [GET] /admin/accounts/
 module.exports.index = async (req, res) => {
@@ -14,4 +20,28 @@ module.exports.index = async (req, res) => {
     pageTitle: "Danh sách tài khoản",
     records: records,
   });
+};
+
+// [GET] /admin/accounts/create
+module.exports.create = async (req, res) => {
+  const roles = await Role.find({
+    deleted: false
+  });
+
+  res.render("admin/pages/accounts/create", {
+    pageTitle: "Tạo mới tài khoản",
+    roles: roles
+  });
+};
+
+// [POST] /admin/accounts/create
+module.exports.createPost = async (req, res) => {
+
+  req.body.token = generateHelper.generateRandomString(30);
+  req.body.password = md5(req.body.password);
+
+  const record = new Account(req.body);
+  await record.save();
+
+  res.redirect(`/${systemConfig.prefixAdmin}/accounts`);
 };
