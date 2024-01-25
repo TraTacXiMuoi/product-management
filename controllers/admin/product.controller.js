2b18e9b4606dbdbf760f13c50c8eda0d6b6e831a
Đặng Phương Nam <61955753+namdp1999@users.noreply.github.com>
2024-01-25 21:43:21 +0700

    2. Thêm logs lịch sử thay đổi sản phẩm (Xóa sản phẩm)

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -108,7 +108,10 @@ module.exports.changeMulti = async (req, res) => {
         _id: { $in: ids }
       }, {
         deleted: true,
-        deletedAt: new Date()
+        deletedBy: {
+          accountId: res.locals.user.id,
+          deletedAt: new Date()
+        }
       });
       req.flash('success', 'Xóa sản phẩm thành công!');
       break;
@@ -145,7 +148,10 @@ module.exports.deleteItem = async (req, res) => {
       _id: id
     }, {
       deleted: true,
-      deletedAt: new Date()
+      deletedBy: {
+        accountId: res.locals.user.id,
+        deletedAt: new Date()
+      }
     });
 
     req.flash('success', 'Xóa sản phẩm thành công!');
