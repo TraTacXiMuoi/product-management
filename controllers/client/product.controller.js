4d831806112d9f93eee72ba99653e268b4e3e1f2
Đặng Phương Nam <61955753+namdp1999@users.noreply.github.com>
2024-01-27 21:52:39 +0700

    5. Trang danh sách sản phẩm bên client (Cơ bản)

--- controllers/client/product.controller.js
+++ controllers/client/product.controller.js
@@ -1,4 +1,5 @@
 const Product = require("../../models/product.model");
+const ProductCategory = require("../../models/product-category.model");
 
 // [GET] /products/
 module.exports.index = async (req, res) => {
@@ -15,6 +16,34 @@ module.exports.index = async (req, res) => {
   }
 
   // console.log(products);
+
+  res.render("client/pages/products/index", {
+    pageTitle: "Danh sách sản phẩm",
+    products: products
+  });
+}
+
+// [GET] /products/:slugCategory
+module.exports.category = async (req, res) => {
+  const slugCategory  = req.params.slugCategory;
+  
+  const category = await ProductCategory.findOne({
+    slug: slugCategory,
+    status: "active",
+    deleted: false
+  });
+
+  const products = await Product.find({
+    product_category_id: category.id,
+    status: "active",
+    deleted: false
+  }).sort({ position: "desc" });
+
+  for (const item of products) {
+    item.priceNew = (item.price * (100 - item.discountPercentage)/100).toFixed(0);
+  }
+  
+  console.log(products);
 
   res.render("client/pages/products/index", {
     pageTitle: "Danh sách sản phẩm",
