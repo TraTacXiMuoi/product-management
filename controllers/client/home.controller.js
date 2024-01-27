26327fbd240b735080b3852fe9823f3f86c3025c
Đặng Phương Nam <61955753+namdp1999@users.noreply.github.com>
2024-01-27 21:22:01 +0700

    4.1. Hiển thị danh sách sản phẩm nổi bật

--- controllers/client/home.controller.js
+++ controllers/client/home.controller.js
@@ -1,6 +1,19 @@
+const Product = require("../../models/product.model");
+
 // [GET] /
 module.exports.index = async (req, res) => {
+  const productsFeatured = await Product.find({
+    featured: "1",
+    status: "active",
+    deleted: false,
+  }).sort({ position: "desc" }).limit(6);
+
+  for (const item of productsFeatured) {
+    item.priceNew = item.price * (100 - item.discountPercentage)/100;
+  }
+
   res.render("client/pages/home/index", {
     pageTitle: "Trang chủ",
+    productsFeatured: productsFeatured
   });
 }
