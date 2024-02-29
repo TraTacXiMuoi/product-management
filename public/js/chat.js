6088afbb3f147e62df9e02d89a528c2eaabc1363
Đặng Phương Nam <61955753+namdp1999@users.noreply.github.com>
2024-02-29 22:46:40 +0700

    1.2. Typing

--- public/js/chat.js
+++ public/js/chat.js
@@ -10,6 +10,7 @@ if(formSendData) {
     if(content) {
       socket.emit("CLIENT_SEND_MESSAGE", content);
       inputContent.value = "";
+      socket.emit("CLIENT_SEND_TYPING", "hidden");
     }
   });
 }
@@ -18,6 +19,7 @@ if(formSendData) {
 // SERVER_SEND_MESSAGE
 socket.on("SERVER_SEND_MESSAGE", (data) => {
   const body = document.querySelector(".chat .inner-body");
+  const elementListTyping = body.querySelector(".inner-list-typing");
   const myId = document.querySelector("[my-id]").getAttribute("my-id");
 
   const div = document.createElement("div");
@@ -35,7 +37,7 @@ socket.on("SERVER_SEND_MESSAGE", (data) => {
     <div class="inner-content">${data.content}</div>
   `;
 
-  body.appendChild(div);
+  body.insertBefore(div, elementListTyping);
   body.scrollTop = body.scrollHeight;
 })
 // End SERVER_SEND_MESSAGE
@@ -63,9 +65,55 @@ if(buttonIcon) {
   const emojiPicker = document.querySelector("emoji-picker");
   const inputChat = document.querySelector(".chat .inner-form input[name='content']");
 
+  // Inser Icon
   emojiPicker.addEventListener('emoji-click', event => {
     const icon = event.detail.unicode;
     inputChat.value = inputChat.value + icon;
   });
+
+  // Show Typing
+  var timeOut;
+
+  inputChat.addEventListener("keyup", () => {
+    socket.emit("CLIENT_SEND_TYPING", "show");
+
+    clearTimeout(timeOut);
+
+    timeOut = setTimeout(() => {
+      socket.emit("CLIENT_SEND_TYPING", "hidden");
+    }, 3000);
+  });
 }
 // End Show Icon Chat
+
+// SERVER_RETURN_TYPING
+const elementListTyping = document.querySelector(".chat .inner-body .inner-list-typing");
+
+socket.on("SERVER_RETURN_TYPING", (data) => {
+  if(data.type == "show") {
+    const existTyping = elementListTyping.querySelector(`.box-typing[user-id="${data.userId}"]`);
+
+    if(!existTyping) {
+      const boxTyping = document.createElement("div");
+      boxTyping.classList.add("box-typing");
+      boxTyping.setAttribute("user-id", data.userId);
+      boxTyping.innerHTML = `
+        <div class="inner-name">${data.fullName}</div>
+        <div class="inner-dots">
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+      `;
+  
+      elementListTyping.appendChild(boxTyping);
+    }
+  } else {
+    const boxTypingRemove = elementListTyping.querySelector(`.box-typing[user-id="${data.userId}"]`);
+
+    if(boxTypingRemove) {
+      elementListTyping.removeChild(boxTypingRemove);
+    }
+  }
+});
+// End SERVER_RETURN_TYPING
