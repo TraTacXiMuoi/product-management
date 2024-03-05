5a95c0f8f59af9dc2985024c66d982bc39603a11
Đặng Phương Nam <61955753+namdp1999@users.noreply.github.com>
2024-03-05 22:56:13 +0700

    2.1. Hiển thị thêm tag online

--- controllers/client/user.controller.js
+++ controllers/client/user.controller.js
@@ -83,12 +83,25 @@ module.exports.loginPost = async (req, res) => {
     user_id: user.id
   });
 
+  await User.updateOne({
+    _id: user.id
+  }, {
+    statusOnline: "online"
+  });
+
   res.redirect("/");
 };
 
 // [GET] /user/logout
 module.exports.logout = async (req, res) => {
   res.clearCookie("tokenUser");
+
+  await User.updateOne({
+    _id: res.locals.user.id
+  }, {
+    statusOnline: "offline"
+  });
+
   res.redirect("/");
 };
 
