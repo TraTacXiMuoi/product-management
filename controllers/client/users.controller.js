2420bffe6dea5e18a70c68cb0ea9b6c305b4ffcc
Đặng Phương Nam <61955753+namdp1999@users.noreply.github.com>
2024-03-05 21:24:14 +0700

    1.6. Chức năng chấp nhận kết bạn

--- controllers/client/users.controller.js
+++ controllers/client/users.controller.js
@@ -12,12 +12,14 @@ module.exports.notFriend = async (req, res) => {
 
   const requestFriends = res.locals.user.requestFriends;
   const acceptFriends = res.locals.user.acceptFriends;
+  const friendsListId = res.locals.user.friendsList.map(item => item.user_id);
 
   const users = await User.find({
     $and: [
       { _id: { $ne: userId } },
       { _id: { $nin: requestFriends } },
-      { _id: { $nin: acceptFriends } }
+      { _id: { $nin: acceptFriends } },
+      { _id: { $nin: friendsListId } }
     ],
     status: "active",
     deleted: false
@@ -69,6 +71,24 @@ module.exports.accept = async (req, res) => {
 
   res.render("client/pages/users/accept", {
     pageTitle: "Lời mời đã nhận",
+    users: users
+  });
+};
+
+// [GET] /users/friends
+module.exports.friends = async (req, res) => {
+  const friendsListId = res.locals.user.friendsList.map(item => item.user_id);
+  
+  const users = await User.find({
+    _id: { $in: friendsListId },
+    status: "active",
+    deleted: false
+  }).select("id fullName avatar");
+
+  console.log(users);
+
+  res.render("client/pages/users/friends", {
+    pageTitle: "Danh sách bạn bè",
     users: users
   });
 };
